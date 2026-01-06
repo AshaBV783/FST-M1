7b832aa000beb6d2aefa21ac60e4309da8762663
AshaBV783 <asha.b.v@ibm.com>
2026-01-06 18:27:19 +0530

    Add files via upload

---
+++ Cucumber/Activities/baseclass.java
@@ -0,0 +1,10 @@
+package stepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class baseclass {
+    // Create a new instance of the Firefox driver
+    protected static WebDriver driver;
+    protected static WebDriverWait wait;
+}
